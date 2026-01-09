87ece0fac46c0bf1cae55b8136f92a2fefc58169
ashwith0111 <kasha.ashwith@gmail.com>
2026-01-09 15:25:54 +0530

    login issue

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -26,9 +26,8 @@ export default function Login() {
         const data = await response.json();
         setError(data.error || "Login failed");
         return;
-      }
-
-      router.push("/main");
+      }    
+      window.location.href='/'
     } catch (err) {
       setError("An error occurred. Please try again.");
     } finally {
